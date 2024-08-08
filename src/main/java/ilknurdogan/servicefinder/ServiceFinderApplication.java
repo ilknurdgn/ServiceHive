1f09e359fafee7edfba26dafb249b22e03f7b91a
İlknur Doğan <ilknurdogan254@gmail.com>
2024-08-08 23:57:35 +0300

    feat: create project and connect database

---
+++ src/main/java/ilknurdogan/servicefinder/ServiceFinderApplication.java
@@ -0,0 +1,13 @@
+package ilknurdogan.servicefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ilknurdogan/servicefinder/ServiceFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ilknurdogan.servicefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
